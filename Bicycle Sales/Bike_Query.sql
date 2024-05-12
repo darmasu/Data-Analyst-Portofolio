1c56c79ba23494266ea02fcb9899f8b04de019cc
darmasu <100006890+darmasu@users.noreply.github.com>
2024-05-12 23:08:49 +0700

    Add files via upload

---
+++ Bicycle Sales/Bike_Query.sql
@@ -0,0 +1,35 @@
+--Join All Table
+with cte as (
+select * from bike_share_yr_0
+union all
+select * from bike_share_yr_1)
+
+select * from cte a
+left join cost_table b
+on a.yr = b.yr
+
+-- Join Specific Collumn
+with cte as (
+select * from bike_share_yr_0
+union all
+select * from bike_share_yr_1)
+
+select 
+dteday, season, a.yr, weekday, hr, rider_type, riders, price, COGS
+from cte a
+left join cost_table b
+on a.yr = b.yr
+
+-- Join Specific Collumn and adding operation
+with cte as (
+select * from bike_share_yr_0
+union all
+select * from bike_share_yr_1)
+
+select 
+dteday, season, a.yr, weekday, hr, rider_type, riders, price, COGS, riders * price as revenue, riders * price - COGS * riders as profit
+from cte a
+left join cost_table b
+on a.yr = b.yr
+
+
